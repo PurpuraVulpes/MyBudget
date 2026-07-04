444407d0be655e54038135c192ff1a6bf8e5f874
PurpuraVulpes <legrasmathis59@gmail.com>
2026-07-04 23:36:47 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,30 +1,20 @@
-const CACHE = 'monbudget-v2';
-const FILES = [
-    './',
-    './index.html',
-    './style.css',
-    './app.js',
-    './manifest.json'
-];
+const CACHE = 'monbudget-v3';
+const FILES = ['./', './index.html', './style.css', './app.js', './firebase-sync.js', './manifest.json'];
 
 self.addEventListener('install', e => {
-    e.waitUntil(
-        caches.open(CACHE).then(c => c.addAll(FILES))
-    );
+    e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
     self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
-    e.waitUntil(
-        caches.keys().then(keys =>
-            Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-        )
-    );
+    e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
     self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
-    e.respondWith(
-        caches.match(e.request).then(r => r || fetch(e.request))
-    );
+    // Don't cache Firebase requests
+    if (e.request.url.includes('firebase') || e.request.url.includes('googleapis')) {
+        return e.respondWith(fetch(e.request));
+    }
+    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
 });
