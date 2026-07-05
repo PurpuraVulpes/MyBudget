cddb795579c37161a92115211afafab2a36baccb
PurpuraVulpes <legrasmathis59@gmail.com>
2026-07-05 11:50:55 +0200

    Upgrade service worker to version 4

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
-const CACHE = 'monbudget-v3';
+const CACHE = 'monbudget-v4';
 const FILES = ['./', './index.html', './style.css', './app.js', './firebase-sync.js', './manifest.json'];
 
-self.addEventListener('install', e => {
-    e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+self.addEventListener('install', function(e) {
+    e.waitUntil(caches.open(CACHE).then(function(c) { return c.addAll(FILES); }));
     self.skipWaiting();
 });
 
-self.addEventListener('activate', e => {
-    e.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))));
+self.addEventListener('activate', function(e) {
+    e.waitUntil(caches.keys().then(function(keys) {
+        return Promise.all(keys.filter(function(k) { return k !== CACHE; }).map(function(k) { return caches.delete(k); }));
+    }));
     self.clients.claim();
 });
 
-self.addEventListener('fetch', e => {
-    // Don't cache Firebase requests
+self.addEventListener('fetch', function(e) {
     if (e.request.url.includes('firebase') || e.request.url.includes('googleapis')) {
         return e.respondWith(fetch(e.request));
     }
-    e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)));
+    e.respondWith(caches.match(e.request).then(function(r) { return r || fetch(e.request); }));
 });
